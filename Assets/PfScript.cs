0c19b447ca25626b70ad464a3da8738cdf09f2cd
Valentine Morin <zm724@nyu.edu>
2018-03-17 22:00:04 -0400

    Continued attempts at making script work. Edited numbers & probabilities to be more focused on floor tiles than more pathmakers.

--- Assets/PfScript.cs
+++ Assets/PfScript.cs
@@ -10,35 +10,54 @@ public class PfScript : MonoBehaviour {
 	public Transform floorPrefab;
 
 	public Transform pathmakerPrefab;
+
+	public float speed = 5f;
+
+	public int pathCounter = 0;
+
+	public static int globalTileCount = 0;
 	
 	// Use this for initialization
 	void Start ()
 	{
-		int pathCounter = 0;
+	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-		if (counter < 50)
+
+		if (pathCounter <= 7 && counter < 50)
 		{
-			counter = Random.Range(0.0f, 1.0f);
-			if (counter <= 0.50f)
 			{
-				transform.Rotate(0f, 90f, 0f);
-			} else if (counter > 0.25f && counter <= 0.90f)
-			{
-				transform.Rotate(0f, -90f, 0f);
-			} else if (counter >= 0.99f)
-			{
-				Instantiate(pathmakerPrefab, transform.position, transform.rotation);
+				counter = Random.Range(0.0f, 1.0f); //start mcfuckin going
+				if (counter <= 0.50f)
+				{
+					transform.Rotate(0f, 90f, 0f);
+				} else if (counter > 0.25f && counter <= 0.90f)
+				{
+					transform.Rotate(0f, -90f, 0f);
+				} else if (counter >= 0.99f) //VERY SMALL chance to spawn a pathmaker
+				{
+					Instantiate(pathmakerPrefab, transform.position, transform.rotation);
+					pathCounter++;
+				}
+				
+				//move pathmaker, destroy it after a short-ish time
+				transform.Translate(0f, 0f, speed);
+				Instantiate(floorPrefab, transform.position, transform.rotation);
+				counter++;
+				globalTileCount++;
+				if (counter >= 25)
+				{
+					Destroy(gameObject);
+				}
 			}
-			transform.Translate(0, 0, 5);
-			Instantiate(floorPrefab, transform.position, transform.rotation);
-			counter++;
-		} else if (counter >= 50)
+		} else if (pathCounter > 7)
 		{
-			Destroy(gameObject);
+			return;
 		}
+		
+			
+
 	}
 }
