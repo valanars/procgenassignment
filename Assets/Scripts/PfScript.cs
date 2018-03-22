b5a773153db96d2298388ffb27b792196f7f4151
Valentine Morin <zm724@nyu.edu>
2018-03-22 11:02:27 -0400

    Edited speed range so tiles always connect

--- Assets/Scripts/PfScript.cs
+++ Assets/Scripts/PfScript.cs
@@ -26,7 +26,7 @@ public class PfScript : MonoBehaviour {
 	void Start ()
 	{
 		maxTiles = Random.Range(1, 100);
-		speed = Random.Range(1, 4);
+		speed = Random.Range(1, 3);
 	}
 	
 	// Update is called once per frame
